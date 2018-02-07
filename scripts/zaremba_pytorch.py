7f033d802f946c261335b66292fe306d7c0e1723
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2018-02-07 15:33:52 +0100

    First version of zaremba_pytorch.py; it probably won't work as it is, though.

--- scripts/zaremba_pytorch.py
+++ scripts/zaremba_pytorch.py
@@ -11,8 +11,45 @@ Pytorch as well. Hence this repository; to find out what's wrong with Pytorch.
 """
 
 import argparse
+import math
+import time
 
+import torch
+import torch.nn as nn
 from torch.autograd import Variable
+
+from pytorch_lm import data
+from pytorch_lm.lstm_pytorch import Lstm
+
+
+class SmallZarembaModel(nn.Module):
+    """"Implements the small model from Zaremba (2014)."""
+    def __init__(self, vocab_size):
+        super(SmallZarembaModel, self).__init__()
+        self.hidden_size = 200
+        self.input_size = 1
+        self.num_layers = 2
+
+        self.encoder = nn.Embedding(vocab_size, self.hidden_size)
+        self.rnn = Lstm(self.input_size, self.hidden_size, self.num_layers)
+        self.decoder = nn.Linear(self.hidden_size, vocab_size)
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.uniform_(-initrange, initrange)  # fill_(0)
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, hidden):
+        emb = self.encoder(input)
+        # self.rnn.flatten_parameters()
+        output, hidden = self.rnn(emb, hidden)
+        decoded = self.decoder(
+            output.view(output.size(0) * output.size(1), output.size(2)))
+        return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
+
+    def init_hidden(self, batch_size):
+        return self.rnn.init_hidden(batch_size)
 
 
 def parse_arguments():
@@ -70,13 +107,179 @@ def get_batch(source, i, bptt, evaluation=False):
     to the seq_len dimension in the LSTM.
     """
     seq_len = min(bptt, len(source) - 1 - i)
-    data = Variable(source[i:i+seq_len], volatile=evaluation)
+    # TODO can we no_grad target as well?
+    if evaluation:
+        with torch.no_grad():
+            data = Variable(source[i:i+seq_len])
+    else:
+        data = Variable(source[i:i+seq_len])
     target = Variable(source[i+1:i+1+seq_len])  # .view(-1))
     return data, target
 
 
+def train(model, corpus, train_data, criterion, epoch, lr, batch_size,
+          num_steps, log_interval):
+    # Turn on training mode which enables dropout.
+    model.train()
+    total_loss = 0
+    start_time = time.time()
+    ntokens = len(corpus.dictionary)
+    hidden = model.init_hidden(batch_size)
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, num_steps)):
+        data, targets = get_batch(train_data, i, num_steps)
+
+        def to_str(f):
+            return corpus.dictionary.idx2word[f]
+
+        # print(data.data.cpu().numpy())
+        # print('DATA\n', np.vectorize(to_str)(data.data.cpu().numpy()))
+        # print(targets.data.cpu().numpy())
+        # Starting each batch, we detach the hidden state from how it was previously produced.
+        # If we didn't, the model would try backpropagating all the way to start of the dataset.
+        hidden = repackage_hidden(hidden)
+        model.zero_grad()
+        output, hidden = model(data, hidden)
+        # print('TARGETS\n', np.vectorize(to_str)(targets.data.cpu().numpy()))
+        # _, indices = output.max(2)
+        # print('OUTPUT\n', np.vectorize(to_str)(indices.data.cpu().numpy()))
+        loss = criterion(output.view(-1, ntokens), targets.view(-1))
+        loss.backward()
+
+        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        # torch.nn.utils.clip_grad_norm(model.parameters(), config.clip)
+        # all_min, all_max, all_sum, all_size = 1000, -1000, 0, 0
+        for name, p in model.named_parameters():
+            data = p.grad.data
+            # shape = data.size()
+            # all_min = all_min if data.min() >= all_min else data.min()
+            # all_max = all_max if data.max() <= all_max else data.max()
+            # all_sum += data.sum()
+            # from functools import reduce
+            # all_size += reduce(lambda a, b: a * b, shape)
+            # print(name, shape, data.min(), data.max(), data.mean(), data.std())
+            p.grad.data.clamp_(-5.0, 5.0)
+            p.data.add_(-1 * lr, p.grad.data)
+        # print('Sum', all_min, all_max, all_sum / all_size)
+        # print()
+        # if batch % log_interval == 0 and batch > 0:
+        #     sys.exit()
+
+        total_loss += loss.data
+
+        if batch % log_interval == 0 and batch > 0:
+            cur_loss = total_loss[0] / log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | '
+                  'ms/batch {:5.2f} | loss {:5.2f} | ppl {:8.2f}'.format(
+                      epoch, batch, len(train_data) // num_steps, lr,
+                      elapsed * 1000 / log_interval, cur_loss, math.exp(cur_loss)),
+                  flush=True)
+            total_loss = 0
+            start_time = time.time()
+
+
+def evaluate(model, corpus, data_source, criterion, batch_size, num_steps):
+    # Turn on evaluation mode which disables dropout.
+    model.eval()
+    total_loss = 0
+    ntokens = len(corpus.dictionary)
+    hidden = model.init_hidden(batch_size)
+    for i in range(0, data_source.size(0) - 1, num_steps):
+        data, targets = get_batch(data_source, i, num_steps, evaluation=True)
+        output, hidden = model(data, hidden)
+        output_flat = output.view(-1, ntokens)
+        targets_flat = targets.view(-1)
+        total_loss += len(data) * criterion(output_flat, targets_flat).data
+        hidden = repackage_hidden(hidden)
+    return total_loss[0] / len(data_source)
+
+
+def repackage_hidden(h):
+    """Wraps hidden states in new Variables, to detach them from their history."""
+    if type(h) == Variable:
+        return Variable(h.data)
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
 def main():
     args = parse_arguments()
+    torch.manual_seed(args.seed)
+
+    if torch.cuda.is_available():
+        if not args.cuda:
+            print('WARNING: You have a CUDA device, so you should probably '
+                  'run with --cuda')
+        else:
+            torch.cuda.manual_seed(args.seed)
+
+    ###############################################################################
+    # Load data
+    ###############################################################################
+
+    corpus = data.Corpus(args.data)
+
+    train_batch_size = 20
+    eval_batch_size = 20
+    num_steps = 20
+    train_data = batchify(corpus.train, train_batch_size, args.cuda)
+    val_data = batchify(corpus.valid, eval_batch_size, args.cuda)
+    test_data = batchify(corpus.test, eval_batch_size, args.cuda)
+
+    ###############################################################################
+    # Build the model
+    ###############################################################################
+
+    vocab_size = len(corpus.dictionary)
+    model = SmallZarembaModel(vocab_size)
+    if args.cuda:
+        model.cuda()
+
+    ###############################################################################
+    # Training code
+    ###############################################################################
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Loop over epochs.
+    orig_lr = 1.0
+    best_val_loss = None
+
+    # At any point you can hit Ctrl + C to break out of training early.
+    try:
+        for epoch in range(1, 13 + 1):
+            lr_decay = 0.5 ** max(epoch - 4, 0.0)
+            lr = orig_lr * lr_decay
+            epoch_start_time = time.time()
+            train(model, corpus, train_data, criterion, epoch,
+                  lr, train_batch_size, num_steps, args.log_interval)
+            val_loss = evaluate(model, corpus, val_data,
+                                criterion, eval_batch_size, num_steps)
+            print('-' * 89)
+            print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+                  'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                             val_loss, math.exp(val_loss)))
+            print('-' * 89)
+            # Save the model if the validation loss is the best we've seen so far.
+            if not best_val_loss or val_loss < best_val_loss:
+                with open(args.save, 'wb') as f:
+                    torch.save(model, f)
+                best_val_loss = val_loss
+    except KeyboardInterrupt:
+        print('-' * 89)
+        print('Exiting from training early')
+
+    # Load the best saved model.
+    with open(args.save, 'rb') as f:
+        model = torch.load(f)
+
+    # Run on test data.
+    test_loss = evaluate(model, corpus, test_data,
+                         criterion, eval_batch_size, num_steps)
+    print('=' * 89)
+    print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
+        test_loss, math.exp(test_loss)))
+    print('=' * 89)
 
 
 if __name__ == '__main__':
