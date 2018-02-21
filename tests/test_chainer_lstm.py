bbabd52c510ff7227ec662f1d5a5fc74d503cdbd
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2018-02-21 13:58:47 +0100

    Tests for the Chainer Lstm.

--- tests/test_chainer_lstm.py
+++ tests/test_chainer_lstm.py
@@ -2,20 +2,26 @@
 # vim: set fileencoding=utf-8 :
 
 import contextlib
+import importlib
 import os
 import unittest
 
+try:
+    chainer = importlib.import_module('chainer')
+    import chainer.functions as F
+    from chainer import Variable
+except ImportError:
+    chainer = None
 import numpy as np
 import tensorflow as tf
-import torch
-from torch.autograd import Variable
 
-import pytorch_lm.lstm_pytorch as pti
+import pytorch_lm.lstm_chainer as chi
 import pytorch_lm.lstm_tf as tfi
 
 
+@unittest.skipIf(chainer is None, "Chainer not found.")
 class TestLstms(unittest.TestCase):
-    """Tests that the two LSTM implementations work alike."""
+    """Tests that the chainer LSTM implementation is the same as the TF one."""
     def setUp(self):
         self.input_size = 5
         self.hidden_size = 4
@@ -24,9 +30,9 @@ class TestLstms(unittest.TestCase):
 
         weight_file = os.path.join(os.path.dirname(__file__), 'lstm.npz')
         if not os.path.isfile(weight_file):
-            pti_lstm = pti.Lstm(self.input_size, self.hidden_size, self.num_layers)
-            pti_d = pti_lstm.save_parameters(prefix='Lstm/')
-            np.savez(weight_file, **pti_d)
+            chi_lstm = chi.Lstm(self.input_size, self.hidden_size, self.num_layers)
+            chi_d = chi_lstm.save_parameters(prefix='Lstm/')
+            np.savez(weight_file, **chi_d)
         self.weights = dict(np.load(weight_file))
 
     @contextlib.contextmanager
@@ -34,7 +40,7 @@ class TestLstms(unittest.TestCase):
                        num_layers=None, batch_size=None, cuda=False):
         """
         Helper method. Creates three objects:
-        - a pytorch Lstm
+        - a chainer Lstm
         - a tensorflow Lstm
         - a session for the latter.
         """
@@ -42,9 +48,9 @@ class TestLstms(unittest.TestCase):
         hidden_size = hidden_size or self.hidden_size
         num_layers = num_layers or self.num_layers
         batch_size = batch_size or self.batch_size
-        pti_lstm = pti.Lstm(input_size, hidden_size, num_layers)
+        chi_lstm = chi.Lstm(input_size, hidden_size, num_layers)
         if cuda:
-            pti_lstm.cuda()
+            chi_lstm.to_gpu()
 
         with tf.Graph().as_default() as graph:
             initializer = tf.random_uniform_initializer(-0.1, 0.1)
@@ -54,74 +60,74 @@ class TestLstms(unittest.TestCase):
 
         with tf.Session(graph=graph) as session:
             session.run(init)
-            yield pti_lstm, tfi_lstm, session
+            yield chi_lstm, tfi_lstm, session
 
-    @unittest.skipUnless(torch.cuda.is_available(),
+    @unittest.skipUnless(chainer.cuda.available,
                          'CUDA test: a GPU is not available.')
     def test_cuda(self):
-        """Tests if the Pytorch network works on the GPU."""
-        with self.__create_lstms(cuda=False) as (pti_lstm, tfi_lstm, session):
-            self.assertFalse(next(pti_lstm.parameters()).is_cuda)
-            pti_lstm.load_parameters(pti_lstm.save_parameters())
-            self.assertFalse(next(pti_lstm.parameters()).is_cuda)
+        """Tests if the Chainer network works on the GPU."""
+        with self.__create_lstms(cuda=False) as (chi_lstm, tfi_lstm, session):
+            self.assertTrue(next(chi_lstm.params())._cpu)
+            chi_lstm.load_parameters(chi_lstm.save_parameters())
+            self.assertTrue(next(chi_lstm.params())._cpu)
 
-        with self.__create_lstms(cuda=True) as (pti_lstm, tfi_lstm, session):
-            self.assertTrue(next(pti_lstm.parameters()).is_cuda)
-            pti_lstm.load_parameters(pti_lstm.save_parameters())
-            self.assertTrue(next(pti_lstm.parameters()).is_cuda)
+        with self.__create_lstms(cuda=True) as (chi_lstm, tfi_lstm, session):
+            self.assertFalse(next(chi_lstm.params())._cpu)
+            chi_lstm.load_parameters(chi_lstm.save_parameters())
+            self.assertFalse(next(chi_lstm.params())._cpu)
 
-    def __assert_parameters_equals(self, pti_lstm, tfi_lstm, session):
+    def __assert_parameters_equals(self, chi_lstm, tfi_lstm, session):
         """Checks if the state vectors of the two LSTM networks are the same."""
         equals = []
-        for l, pti_cell in enumerate(pti_lstm.layers):
+        for l, chi_cell in enumerate(chi_lstm.layers):
             tfi_cell = tfi_lstm.layers[l]
-            for name, _ in pti_cell.named_parameters():
-                pti_value = getattr(pti_cell, name).data.cpu().numpy()
-                tfi_value = session.run(getattr(tfi_cell, name))
-                equals.append(np.allclose(pti_value, tfi_value))
+            for name, _ in chi_cell.namedparams():
+                chi_value = F.copy(getattr(chi_cell, name[1:]), -1).data
+                tfi_value = session.run(getattr(tfi_cell, name[1:]))
+                equals.append(np.allclose(chi_value, tfi_value))
             self.assertTrue(all(equals))
 
-    def __test_data_from_pytorch_to_tf(self, cuda):
+    def __test_data_from_chainer_to_tf(self, cuda):
         with self.__create_lstms(batch_size=4, cuda=cuda) as (
-            pti_lstm, tfi_lstm, session
+            chi_lstm, tfi_lstm, session
         ):
-            pti_d = pti_lstm.save_parameters(prefix='Lstm/')
-            tfi_lstm.load_parameters(session, pti_d)
+            chi_d = chi_lstm.save_parameters(prefix='Lstm/')
+            tfi_lstm.load_parameters(session, chi_d)
 
-            self.__assert_parameters_equals(pti_lstm, tfi_lstm, session)
+            self.__assert_parameters_equals(chi_lstm, tfi_lstm, session)
 
-    def test_data_from_pytorch_to_tf(self):
-        """Tests data transfer from the pytorch LSTM to the tf one."""
+    def test_data_from_chainer_to_tf(self):
+        """Tests data transfer from the chainer LSTM to the tf one."""
         with self.subTest(name='cpu'):
-            self.__test_data_from_pytorch_to_tf(False)
+            self.__test_data_from_chainer_to_tf(False)
         with self.subTest(name='gpu'):
-            if not torch.cuda.is_available():
+            if not chainer.cuda.available:
                 self.skipTest('CUDA test: a GPU is not available.')
-            self.__test_data_from_pytorch_to_tf(True)
+            self.__test_data_from_chainer_to_tf(True)
 
-    def __test_data_from_tf_to_pytorch(self, cuda):
+    def __test_data_from_tf_to_chainer(self, cuda):
         with self.__create_lstms(batch_size=4, cuda=cuda) as (
-            pti_lstm, tfi_lstm, session
+            chi_lstm, tfi_lstm, session
         ):
             tfi_d = tfi_lstm.save_parameters(session)
-            pti_lstm.load_parameters(tfi_d, prefix='Lstm/')
+            chi_lstm.load_parameters(tfi_d, prefix='Lstm/')
 
-            self.__assert_parameters_equals(pti_lstm, tfi_lstm, session)
+            self.__assert_parameters_equals(chi_lstm, tfi_lstm, session)
 
-    def test_data_from_tf_to_pytorch(self):
-        """Tests data transfer from the tf LSTM to the pytorch one."""
+    def test_data_from_tf_to_chainer(self):
+        """Tests data transfer from the tf LSTM to the chainer one."""
         with self.subTest(name='cpu'):
-            self.__test_data_from_tf_to_pytorch(False)
+            self.__test_data_from_tf_to_chainer(False)
         with self.subTest(name='gpu'):
-            if not torch.cuda.is_available():
+            if not chainer.cuda.available:
                 self.skipTest('CUDA test: a GPU is not available.')
-            self.__test_data_from_tf_to_pytorch(True)
+            self.__test_data_from_tf_to_chainer(True)
 
     def __test_sequence_tagging(self, cuda):
         """Tests sequence tagging (i.e. the output)."""
-        with self.__create_lstms(cuda=cuda) as (pti_lstm, tfi_lstm, session):
+        with self.__create_lstms(cuda=cuda) as (chi_lstm, tfi_lstm, session):
             tfi_lstm.load_parameters(session, self.weights)
-            pti_lstm.load_parameters(self.weights, prefix='Lstm/')
+            chi_lstm.load_parameters(self.weights, prefix='Lstm/')
 
             # Input
             input_np = np.array(
@@ -132,35 +138,38 @@ class TestLstms(unittest.TestCase):
                 ],
                 dtype=np.float32
             )
-            pti_input = Variable(torch.FloatTensor(input_np))
+            chi_input = Variable(input_np)
             if cuda:
-                pti_input = pti_input.cuda()
+                chi_input = chi_input.to_gpu()
             tfi_input = tf.placeholder(tf.float32, input_np.shape)
 
             # Target (arithmetic mean)
             target_np = np.array([[1.5, 3, 2.5], [1, 2, 3], [1, 2, 3]], dtype=np.float32)
-            pti_target = Variable(torch.FloatTensor(target_np))
+            chi_target = Variable(target_np)
             if cuda:
-                pti_target = pti_target.cuda()
+                chi_target = chi_target.to_gpu()
             tfi_target = tf.placeholder(tf.float32, target_np.shape)
 
             # Initial states
-            pti_hidden = pti_lstm.init_hidden(self.batch_size)
-            tfi_hidden_np = [[v.data.cpu().numpy() for v in l] for l in pti_hidden]
+            chi_hidden = chi_lstm.init_hidden(self.batch_size)
+            tfi_hidden_np = [[F.copy(v, -1).data for v in l] for l in chi_hidden]
             tfi_init_state = tfi_lstm.init_hidden()
             tfi_output, tfi_final_state = tfi_lstm(tfi_input, tfi_init_state)
 
-            # Pytorch
-            pti_lstm.zero_grad()
-            pti_output, pti_final_state = pti_lstm.forward(pti_input, pti_hidden)
-            pti_loss = (pti_output.mean(2) - pti_target).norm(2)
-            pti_loss.backward()
-            pti_final_state_np = [[a.data.cpu().numpy() for a in l]
-                                  for l in pti_final_state]
-            pti_loss_np = pti_loss.data[0]
-            pti_grads_dict = {l: {name: p.grad.data.cpu().numpy()
-                                  for name, p in layer.named_parameters()}
-                              for l, layer in enumerate(pti_lstm.layers)}
+            optimizer = chainer.optimizers.SGD(lr=1.0)
+            optimizer.setup(chi_lstm)
+
+            # Chainer
+            chi_lstm.cleargrads()
+            chi_output, chi_final_state = chi_lstm(chi_input, chi_hidden)
+            chi_loss = F.sqrt(F.sum((F.mean(chi_output, 2) - chi_target) ** 2))
+            chi_loss.backward()
+            chi_final_state_np = [[F.copy(a, -1).data for a in l]
+                                  for l in chi_final_state]
+            chi_loss_np = np.asscalar(F.copy(chi_loss, -1).data)
+            chi_grads_dict = {l: {name: F.copy(p.grad, -1).data
+                                  for name, p in layer.namedparams()}
+                              for l, layer in enumerate(chi_lstm.layers)}
 
             # Tensorflow
             tfi_loss = tf.norm(tf.reduce_mean(tfi_output, axis=2) - tfi_target, 2)
@@ -176,40 +185,42 @@ class TestLstms(unittest.TestCase):
                 tfi_feed_dict)
             tfi_grads_dict = {
                 l: {
-                    name: tfi_grads_list[tfi_grad_vars.index(getattr(layer, name))]
-                    for name in pti_grads_dict[l]
+                    name: tfi_grads_list[tfi_grad_vars.index(getattr(layer, name[1:]))]
+                    for name in chi_grads_dict[l]
                 }
                 for l, layer in enumerate(tfi_lstm.layers)
             }
 
             # The tests
             with self.subTest(name='state'):
-                for layer in range(len(pti_final_state_np)):
+                for layer in range(len(chi_final_state_np)):
                     for hc in range(2):
                         self.assertTrue(np.allclose(
-                            pti_final_state_np[layer][hc],
+                            chi_final_state_np[layer][hc],
                             tfi_final_state_np[layer][hc],
                         ))
 
             with self.subTest(name='loss'):
-                self.assertTrue(np.allclose(tfi_loss_np, pti_loss_np))
+                self.assertTrue(np.allclose(tfi_loss_np, chi_loss_np))
 
             with self.subTest(name='gradients'):
-                for layer, pti_grads in pti_grads_dict.items():
-                    for name, pti_value in pti_grads.items():
+                for layer, chi_grads in chi_grads_dict.items():
+                    for name, chi_value in chi_grads.items():
                         self.assertTrue(np.allclose(
-                            pti_value, tfi_grads_dict[layer][name]))
+                            chi_value, tfi_grads_dict[layer][name]))
 
-            # Apply the loss on both
-            for p in pti_lstm.parameters():
-                p.data.add_(-p.grad.data)
-            self.__assert_parameters_equals(pti_lstm, tfi_lstm, session)
+            # Apply the loss in Chainer
+            optimizer.update()
+            # For simple SGD, the 3 optimizer lines are equivalent to
+            # for p in chi_lstm.params():
+            #     p.data -= p.grad
+            self.__assert_parameters_equals(chi_lstm, tfi_lstm, session)
 
     def test_sequence_tagging_cpu(self):
         """Tests sequence tagging (i.e. the output) on the CPU."""
         self.__test_sequence_tagging(False)
 
-    @unittest.skipUnless(torch.cuda.is_available(),
+    @unittest.skipUnless(chainer.cuda.available,
                          'CUDA test: a GPU is not available.')
     def test_sequence_tagging_gpu(self):
         """Tests sequence tagging (i.e. the output) on the GPU."""
